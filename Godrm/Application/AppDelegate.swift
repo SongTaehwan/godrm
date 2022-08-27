f5d284354043d8488c305bc21f8ac63d570a3a98
Song TaeHwan <axoghks@gmail.com>
2022-08-27 16:55:47 +0900

    build: Handle xcode version update

--- Godrm/Application/AppDelegate.swift
+++ Godrm/Application/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
