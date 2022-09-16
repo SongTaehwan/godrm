1404d3ca117e03cef96614751c2e2487c3497a0f
Song TaeHwan <axoghks@gmail.com>
2022-09-16 19:51:29 +0900

    feat: #7 Declare OnboardingItem as entity

---
+++ Godrm/Domain/Entities/OnboardingItem.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingItem.swift
+//  Godrm
+//
+//  Created by 송태환 on 2022/09/16.
+//
+
+import Foundation
+
+struct OnboardingItem: Equatable {
+    let title: String
+    let image: Data
+}
